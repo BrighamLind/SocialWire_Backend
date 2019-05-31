27b572d75cc786909bc15126bbbdf266d0ca8a2c
BrighamLind <brighamlind@gmail.com>
2019-05-31 14:44:55 -0600

    Installed flask-heroku, gunicorn, and added Procfile

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
+from flask_heroku import Heroku
 import os
 
 app = Flask(__name__)
+heroku - Heroku(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
